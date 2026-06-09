31ba5e409ecad505684cd3000d8f908259e125b4
swissneo85 <130314029+swissneo85@users.noreply.github.com>
2026-06-09 13:28:40 +0200

    Fix start.sh: auto-create database on container start, show errors

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-php artisan migrate --force 2>/dev/null || true
-php artisan db:seed --force 2>/dev/null || true
+#!/usr/bin/env sh
+echo "⚡️ Migration ..."
+php artisan migrate --force 2>&1 || echo "Migration fehleschlagen, vermutlich normal bei erstem Start."
+
+echo "⚇️ Seeding ..."
+php artisan db:seed --force 2>&1 || echo "Seeding fehlschlagen, vermutlich normal bei erstem Start."
+
+echo "⚡️ Starte Nginx + PHP-FPM ..."
 exec /usr/bin/supervisord -c /etc/supervisord.conf
